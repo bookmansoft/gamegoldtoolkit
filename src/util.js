67f078ad55ccf36c62f3fc54a1aea6ee0fee5134
bianque <bianque@qq.com>
2018-11-06 19:55:05 +0800

    ...

---
+++ src/util.js
@@ -0,0 +1,149 @@
+const createHmac = require('create-hmac/browser');
+
+/**
+ * 客户端请求返回值，统一定义所有的错误码，每100个为一个大类
+ */
+const ReturnCode = {
+    Success: 0,             //操作成功
+};
+
+const CommMode = {
+    ws: "webSocket",    //Web Socket
+    get: "get",         //HTTP GET
+    post: "post",       //HTTP POST
+}
+
+function Base64() {
+	// private property
+	const _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+ 
+	// private method for UTF-8 encoding
+	const _utf8_encode = function (string) {
+		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+		for (var n = 0; n < string.length; n++) {
+			var c = string.charCodeAt(n);
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			} else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			} else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+ 
+		}
+		return utftext;
+	}
+ 
+	// private method for UTF-8 decoding
+	const _utf8_decode = function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+		while ( i < utftext.length ) {
+			c = utftext.charCodeAt(i);
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			} else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			} else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+		}
+		return string;
+	}
+
+  // public method for encoding
+	this.encode = function (input) {
+		var output = "";
+		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+		var i = 0;
+		input = _utf8_encode(input);
+		while (i < input.length) {
+			chr1 = input.charCodeAt(i++);
+			chr2 = input.charCodeAt(i++);
+			chr3 = input.charCodeAt(i++);
+			enc1 = chr1 >> 2;
+			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+			enc4 = chr3 & 63;
+			if (isNaN(chr2)) {
+				enc3 = enc4 = 64;
+			} else if (isNaN(chr3)) {
+				enc4 = 64;
+			}
+			output = output +
+			_keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+			_keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+		}
+		return output;
+	}
+ 
+	// public method for decoding
+	this.decode = function (input) {
+		var output = "";
+		var chr1, chr2, chr3;
+		var enc1, enc2, enc3, enc4;
+		var i = 0;
+		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+		while (i < input.length) {
+			enc1 = _keyStr.indexOf(input.charAt(i++));
+			enc2 = _keyStr.indexOf(input.charAt(i++));
+			enc3 = _keyStr.indexOf(input.charAt(i++));
+			enc4 = _keyStr.indexOf(input.charAt(i++));
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
+			output = output + String.fromCharCode(chr1);
+			if (enc3 != 64) {
+				output = output + String.fromCharCode(chr2);
+			}
+			if (enc4 != 64) {
+				output = output + String.fromCharCode(chr3);
+			}
+		}
+		output = _utf8_decode(output);
+		return output;
+	}
+}
+
+/**
+ * 利用HMAC算法，以及令牌固定量和令牌随机量，计算访问令牌
+ * @param {*} token
+ * @param {*} random
+ */
+function signHMAC(token, random) {
+    var hmac = createHmac('sha256', random);
+    let sig = hmac.update(token).digest('hex');
+    return sig;
+}
+
+function now() {
+    return Math.floor(ms() / 1000);
+  };
+  
+  /**
+   * Get current time in unix time (milliseconds).
+   * @returns {Number}
+   */
+  
+function ms() {
+    return Date.now();
+};
+
+exports.now = now;
+exports.ms = ms;
+exports.Base64 = Base64;
+exports.signHMAC = signHMAC;
+exports.ReturnCode = ReturnCode;
+exports.CommMode = CommMode;
+exports.createHmac = createHmac;
